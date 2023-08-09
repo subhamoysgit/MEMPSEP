67209a3ac061c723ff0170b1ae757162a7bc50e9
subhamoysgit <sc8473@gmail.com>
2023-08-08 19:28:39 -0600

    added main()

--- model_inference.py
+++ model_inference.py
@@ -1,13 +1,14 @@
 import numpy as np
 import pandas as pd
 import pickle
+import os
+from sklearn.preprocessing import MinMaxScaler
 from probability_calibration import probability_calibration
+from model import MEMPSEP
+
 DATA_DIR = '/d1/sep_data/'
 MODEL_DIR = '/d1/sep_data/models/'
 MNAME = 'MUFWXE'
-input_dict = {'M': int('M' in MNAME), 'U': int('U' in MNAME),
-              'X': int('X' in MNAME), 'E': int('E' in MNAME),
-              'W': int('W' in MNAME)}
 
 
 def infer(model, identifier, input_dict, pn, df_INSITU, dw, ds, de):
@@ -89,3 +90,76 @@ def infer(model, identifier, input_dict, pn, df_INSITU, dw, ds, de):
         prob = p_cal.calibrateProbability(n_sel=20)
         infr.append(prob[0])
     return infr
+
+
+def main():
+    input_dict = {'M': int('M' in MNAME), 'U': int('U' in MNAME),
+                  'X': int('X' in MNAME), 'E': int('E' in MNAME),
+                  'W': int('W' in MNAME)}
+    filename_o = 'all_ML_parameters_int_flux.csv'
+    df_o = pd.read_csv(DATA_DIR + filename_o, header=1)
+
+    insitu_cols = ['SW Temp', 'SW Velocity', 'SW Density',
+                   'IMF B', 'IMF Bx', 'IMF By', 'IMF Bz',
+                   'Fe/O Lo', 'Fe/O Hi', 'H Lo', 'H Hi',
+                   'O Lo', 'O Hi', 'Fe Lo', 'Fe Hi']
+
+    etype0 = list(df_o['event_type'] == 0)
+    etype1 = list(df_o['event_type'] == 1)
+
+    for col in insitu_cols:
+        ind = np.logical_and(list(df_o[col] == -9999), etype0)
+        ind_c = np.logical_and(list(df_o[col] != -9999), etype0)
+        df_o.loc[ind, col] = df_o[col][ind_c].sample(np.sum(ind), replace=True,
+                                                     random_state=239).values
+
+    for col in insitu_cols:
+        ind = np.logical_and(list(df_o[col] == -9999), etype1)
+        ind_c = np.logical_and(list(df_o[col] != -9999), etype1)
+        df_o.loc[ind, col] = df_o[col][ind_c].sample(np.sum(ind),
+                                                     replace=True,
+                                                     random_state=239).values
+
+    scale = MinMaxScaler()
+    for col in insitu_cols:
+        if col in ['IMF Bx', 'IMF By', 'IMF Bz']:
+            df_o[col] = df_o[col]/np.max(np.abs(df_o[col]))
+        else:
+            df_o[col] = pd.DataFrame(scale.fit_transform(df_o[[col]].values),
+                                     columns=[col], index=df_o.index)
+
+    df_INSITU = df_o[insitu_cols]
+    pn = list(df_o['FlrOnset'])
+    pn = [d[:4]+d[5:7]+d[8:10]+d[11:13]+d[14:16] for d in pn]
+
+    dw = []
+    for _, _, files in os.walk(DATA_DIR + 'wind_waves/'):
+        for name in files:
+            dw.append(name[3:-len('.p')])
+
+    ds = []
+    for _, _, files in os.walk(DATA_DIR + 'x_ray/'):
+        for name in files:
+            ds.append(name[:12])
+
+    de = []
+    for _, _, files in os.walk(DATA_DIR + 'electrons_L1/'):
+        for name in files:
+            de.append(name[:12])
+
+    model = MEMPSEP(sz=256)
+    QUERY_IDENTIFIERS = ['201401071804', '201707140107',
+                         '201709042028', '201709061153',
+                         '201709101535']
+
+    for identifier in QUERY_IDENTIFIERS:
+        if identifier in pn:
+            m = infer(model, identifier, input_dict, pn, df_INSITU, dw, ds, de)
+            m = np.array(m)
+            print(np.round(m, 2))
+        else:
+            print('not in list')
+
+
+if __name__ == '__main__':
+    main()
